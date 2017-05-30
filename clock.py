8c77f5ea6932b7a89266970d6489cd4b66988b5b
Galingale <a.vangulick@hotmail.com>
2017-05-30 23:06:39 +0200

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,8 +1,3 @@
-import logging
-logging.basicConfig(level=logging.INFO, format=' %(asctime)s -%(levelname)s - %(message)s')
-#logging.disable(logging.CRITICAL)
-
-
 class Clock:
 
     def __init__(self, hours, minutes):
@@ -11,19 +6,11 @@ class Clock:
         assert (isinstance(hours, int))
         assert (isinstance(minutes, int))
 
-        logging.info("Hours: " + str(hours) + " Minutes: " + str(minutes))
-
         self.hours = hours
         self.minutes = minutes
 
-        logging.info("(Before conversion) Hours: " + str(self.hours))
-        logging.info("(Before conversion) Minutes: " + str(self.minutes))
-
         # Convert hours and minutes so they roll over correctly
         self.convert()
-
-        logging.info("(After conversion) Hours: " + str(self.hours))
-        logging.info("(After conversion) Minutes: " + str(self.minutes))
 
     def __str__(self):
         return self.get_time()
@@ -42,17 +29,10 @@ class Clock:
         self.minutes = self.minutes % 60
         self.hours = self.hours % 24
 
-        logging.info("(Before adding) Time: " + self.get_time())
-
         return self.get_time()
 
     def add(self, more_minutes):
         self.minutes += more_minutes
         self.convert()
 
-        logging.info("(After adding) Time: " + self.get_time())
-
         return self.get_time()
-
-clock = Clock(7274, -682)
-print(clock)
